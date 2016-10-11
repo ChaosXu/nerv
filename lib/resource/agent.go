ccaeb221d3437797ff0b3e89d614e19d89853552
ChaosXu <jun.chaos@gmail.com>
2016-10-11 22:11:06 +0800

    Add comment

--- lib/resource/agent.go
+++ lib/resource/agent.go
@@ -6,27 +6,33 @@ func init() {
 	Models["Agent"] = &Agent{}
 }
 
+//Agent is a proxy for control the process instance on the host
 type Agent struct {
 
 }
 
+//Create a process instance on the host
 func (p *Agent) Create() error {
 	log.LogCodeLine()
 	return nil
 }
 
+//Delete the process instance on the host
 func (p *Agent) Delete() error {
 	return nil
 }
 
+//Start the process instance on the host
 func (p *Agent) Start() error {
 	return nil
 }
 
+//Stop the process instance on the host
 func (p *Agent) Stop() error {
 	return nil
 }
 
+//Setup the process instance. eg. config
 func (p *Agent) Setup() error {
 	return nil
 }
