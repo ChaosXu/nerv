948a148ba232ad7d2cce8343f818f6f10a928b62
ChaosXu <jun.chaos@gmail.com>
2016-10-18 17:44:32 +0800

    Fix create.sh

--- cmd/file/scripts/nerv/Agent/create.sh
+++ cmd/file/scripts/nerv/Agent/create.sh
@@ -4,30 +4,19 @@ PID_FILE=agent.pid
 
 function create() {
     echo $(pwd)
-    wget $PKG_URL/$PKG.tar.gz
-    tar -xf $ROOT/$PKG.tar.gz 
-    cd $ROOT/$PKG/bin
-    ./app start
-}
-
-function check_pid() {
-    if [ -f $PID_FILE ]; then
-        pid=`cat $PID_FILE`
-        if [ -n $pid ]; then
-            running=`ps -p $pid|grep -v "PID TTY" |wc -l`
-            return $running
-        fi
-    fi
-    return 0
+    $APP stop
+    wget $PKG_URL
+    tar -xf $ROOT/$PKG 
+    $APP start
 }
 
 if [ "$PKG_URL" == "" ]; then
     echo {\"error\":\"PKG_URL is empty\"}
-elif [ "$PKG" == "" ]; then 
-    echo {\"error\":\"PKG is empty\"}
 elif [ "$ROOT" == ""  ]; then 
     echo {\"error\":\"ROOT is empty\"}
 else
+    PKG=${PKG_URL##*/}
+    APP=$ROOT/${PKG%%.*}/bin/app
     cd $ROOT
     create    
 fi
--- test/agent/create_test.sh
+++ test/agent/create_test.sh
@@ -1,2 +1,2 @@
 CREATE=../../cmd/file/scripts/nerv/Agent/create.sh
-export PKG_URL=http://localhost:3332/pkg PKG=agent ROOT=$(pwd) && $CREATE
+export PKG_URL=http://localhost:3332/pkg/agent.tar.gz ROOT=$(pwd) && $CREATE
