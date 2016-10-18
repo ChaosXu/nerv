ad0c71dc68b328a36ac812947804bb2d76b1ce93
ChaosXu <jun.chaos@gmail.com>
2016-10-18 16:04:05 +0800

    file add pkg

--- Makefile
+++ Makefile
@@ -1,24 +1,24 @@
 export VERSION=0.0.1
 CMD_DIR=cmd
+RELEASE_ROOT=release/nerv
+PKG_AGENT=agent.tar.gz
 
 all : build
-	@echo "----package----"
-	rm -rf release/nerv.tar.gz
-	cd release && tar -zcvf nerv.tar.gz nerv
-	@echo "----package complete----"
+	
 
 build : server agent file
+	cp $(RELEASE_ROOT)/$(PKG_AGENT) $(RELEASE_ROOT)/file/pkg 
 	@echo "----build complete----"
 
  
 server :
 	@echo "----build server----"
-	cd $(CMD_DIR)/server && make server
+	cd $(CMD_DIR)/server && make
 
 agent :
 	@echo "----build agent----"
-	cd $(CMD_DIR)/agent && make agent
+	cd $(CMD_DIR)/agent && make
 
 file :
 	@echo "----build file----"
-	cd $(CMD_DIR)/file && make file
+	cd $(CMD_DIR)/file && make
--- cmd/file/scripts/nerv/Agent/create.sh
+++ cmd/file/scripts/nerv/Agent/create.sh
@@ -1,14 +1,37 @@
 #!/usr/bin/env bash
+
+PID_FILE=agent.pid
+
 function create() {
-    wget PKG_URL
+    echo $(pwd)
+    download
+    tar -zcvf $PKG $ROOT 
 }
 
 function download() {
-    echo "download"
+    wget PKG_URL
+    check_pid
+
+}
+
+function check_pid() {
+    if [ -f $PID_FILE ];then
+        pid=`cat $PID_FILE`
+        if [ -n $pid ]; then
+            running=`ps -p $pid|grep -v "PID TTY" |wc -l`
+            return $running
+        fi
+    fi
+    return 0
 }
 
 if [ "$PKG_URL" == "" ]; then
     echo {\"error\":\"PKG_URL is empty\"}
+elif [ "$PKG" == "" ]; then 
+    echo {\"error\":\"PKG is empty\"}
+elif [ "$ROOT" == ""  ]; then 
+    echo {\"error\":\"ROOT is empty\"}
 else
+    cd $ROOT
     create    
 fi
