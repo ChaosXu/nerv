862ff541aa0efc592a5eb4ca5f380d9b8b90ced1
ChaosXu <jun.chaos@gmail.com>
2016-10-24 17:38:03 +0800

    Export args to shell

--- cmd/agent/shell/shell.go
+++ cmd/agent/shell/shell.go
@@ -4,6 +4,7 @@ import (
 	"os/exec"
 	"github.com/ChaosXu/nerv/lib/rpc"
 	"fmt"
+	"log"
 )
 
 func init() {
@@ -20,10 +21,18 @@ type RemoteScript struct {
 }
 //Execute a script in the host of the agent
 func (p *RemoteShell) Execute(script *RemoteScript, response *string) error {
-	out, err := exec.Command("/bin/bash","-c",script.Content).Output()
+	export := ""
+	for k, v := range script.Args {
+		export = export + fmt.Sprintf(" %s=%s", k, v)
+	}
+	shell := "export " + export + " && " + script.Content
+	log.Println(shell)
+
+	out, err := exec.Command("/bin/bash","-c",shell).Output()
 	if err != nil {
 		return err
 	}
-	fmt.Println(string(out))
+	log.Println(string(out))
+
 	return nil
 }
