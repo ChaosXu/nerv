53e918ab3e68b9c2dbafdbe3d1ecaab784ba2365
ChaosXu <jun.chaos@gmail.com>
2016-11-10 23:54:24 +0800

    gulp-connect

---
+++ webui/console/app/dashboard/app.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+@Component({
+    selector: 'nerv-app-dashboard',
+    templateUrl: 'app/dashboard/app.html', 
+})
+export class DashboardApp {
+
+}
---
+++ webui/console/app/monitor/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+
+@Component({
+    selector: 'nerv-app-monitor',
+    templateUrl: 'app/monitor/app.html', 
+})
+export class MonitorApp {
+
+}
