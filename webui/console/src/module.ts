43a4ab578f3bc99740b33301357859ed22f1eda1
ChaosXu <jun.chaos@gmail.com>
2016-11-10 10:38:33 +0800

    Remove the routing module

--- webui/console/src/module.ts
+++ webui/console/src/module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule,Routes } from '@angular/router';
 import { UserModule } from './app/user/module';
 import { DashboardModule } from './app/dashboard/module';
 import { MonitorModule } from './app/monitor/module';
@@ -8,13 +8,15 @@ import { Application } from './ui/application';
 import { StartMenu } from './ui/startmenu';
 import { Dock } from './ui/dock';
 import { CatalogService } from './service/catalog';
-import { Routing } from './routing';
 
+const routes: Routes = [
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [
     BrowserModule,
-    Routing,
+    RouterModule.forRoot(routes),
     DashboardModule,
     UserModule,    
     MonitorModule
