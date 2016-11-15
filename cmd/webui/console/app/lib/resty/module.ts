41ec9d797b0172a2a5b98d41972679d9ac0cd943
ChaosXu <jun.chaos@gmail.com>
2016-11-15 23:28:56 +0800

    Rest service

---
+++ cmd/webui/console/app/lib/resty/module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RestyService } from './resty.service';
+
+
+@NgModule({
+    imports: [
+        HttpModule
+    ],
+    providers: [RestyService]
+})
+export class DbModule { }
---
+++ cmd/webui/console/app/lib/resty/resty.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+
+@Injectable()
+export class RestyService {
+    private headers = new Headers({ 'Content-Type': 'application/json' });
+
+    constructor(private http: Http) { }
+
+    find(type: string): Promise<any> {
+        const url = `api/objs/${type}`;
+        return this.http.get(url, { headers: this.headers })
+            .toPromise()
+            .then(response => {
+                var body = response.json();
+                console.log(body);
+                return body;
+            })
+            .catch(this.error);
+    }
+
+    getOne(type: string, id: number): Promise<any> {
+        const url = `api/objs/${type}/${id}`;
+        return this.http.get(url, { headers: this.headers })
+            .toPromise()
+            .then(response => response.json())
+            .catch(this.error);
+    }
+
+    create(type: string, instance: any): Promise<any> {
+        const url = `api/objs/${type}`;
+        var body = JSON.stringify(instance);
+        console.log(body)
+        return this.http.post(url, body, { headers: this.headers })
+            .toPromise()
+            .then(response => response.json())
+            .catch(this.error);
+    }
+
+    update(type: string, id: number, instance: any): Promise<any> {
+        const url = `api/objs/${type}/${id}`;
+        var body = JSON.stringify(instance);
+        console.log(body)
+        return this.http.put(url, body, { headers: this.headers })
+            .toPromise()
+            .then(response => response.json())
+            .catch(this.error);
+    }
+
+    remove(type: string, id: number): Promise<{ type: string, id: number }> {
+        const url = `api/objs/${type}/${id}`;
+        return this.http.delete(url, { headers: this.headers })
+            .toPromise()
+            .then(() => { return { type: type, id: id } })
+            .catch(this.error);
+    }
+
+    private error(error: any): Promise<any> {
+        return Promise.reject(error);
+    }
+}
