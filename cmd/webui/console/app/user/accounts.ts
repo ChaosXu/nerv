bee74d170017253ab05013979072554f46122233
ChaosXu <jun.chaos@gmail.com>
2016-11-16 23:46:54 +0800

    Clean code

--- cmd/webui/console/app/user/accounts.ts
+++ cmd/webui/console/app/user/accounts.ts
@@ -21,30 +21,4 @@ export class AccountsComponent extends FormsBaseComponent{
             type:'Account'
         });
     }
-
-    // ngOnInit(): void {
-    //     this.resty.find('Account')
-    //         .then(response => this.accounts = response.data)
-    //         .catch(this.error);
-    // }
-
-    // onAdd(): void {
-    //     this.router.navigate(['/user/account/add']);
-    // }
-
-    // onShow(item: {}): void {
-    //     this.router.navigate(['/user/account', item['ID']]);
-    // }
-
-    // onEdit(item: {}): void {
-    //     this.router.navigate(['/user/account', item['ID'], 'edit']);
-    // }
-
-    // onRemove(item: {}): void {
-
-    // }
-
-    // private error(error: any) {
-    //     return alert(error.text());
-    // }
 }
