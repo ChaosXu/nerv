b0e4780dea662b9bc7ac626c0d3d6cf60b134f30
ChaosXu <jun.chaos@gmail.com>
2017-01-20 11:27:14 +0800

    add a reverse proxy in webui

--- cmd/webui/main.go
+++ cmd/webui/main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
-
 	"os"
-
 	"github.com/ChaosXu/nerv/lib/env"
 	"github.com/pressly/chi"
+	"github.com/ChaosXu/nerv/cmd/webui/proxy"
 )
 
 var (
@@ -19,12 +18,36 @@ func main() {
 	log.Println("Version:" + Version)
 	env.Init()
 
-	port := env.Config().GetMapString("http","port")
+	port := env.Config().GetMapString("http", "port")
 	if port == "" {
 		log.Fatalln("http_port isn't setted")
 	}
 
 	r := chi.NewRouter()
+	for source, target := range env.Config().GetMap("proxies") {
+		remoteUrl, ok := target.(string)
+		if !ok {
+			log.Fatalf("get remote url failed. source url:%s\n", source)
+		}
+		serverProxy, err := proxy.NewProxy(remoteUrl)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		r.Route(source, func(r chi.Router) {
+			r.Post("/*", func(w http.ResponseWriter, r *http.Request) {
+				serverProxy.Handle(w, r)
+			})
+			r.Get("/*", func(w http.ResponseWriter, r *http.Request) {
+				serverProxy.Handle(w, r)
+			})
+			r.Delete("/*", func(w http.ResponseWriter, r *http.Request) {
+				serverProxy.Handle(w, r)
+			})
+			r.Put("/*", func(w http.ResponseWriter, r *http.Request) {
+				serverProxy.Handle(w, r)
+			})
+		})
+	}
 
 	for url, file := range env.Config().GetMap("files") {
 		log.Printf("file router: %s -> %s", url, file)
@@ -33,3 +56,4 @@ func main() {
 
 	log.Fatalln(http.ListenAndServe(":" + port, r))
 }
+
---
+++ cmd/webui/proxy/Proxy.go
@@ -0,0 +1,25 @@
+package proxy
+
+import (
+	"net/url"
+	"net/http"
+	"net/http/httputil"
+)
+
+// Proxy config
+type Proxy struct {
+	rp *httputil.ReverseProxy
+}
+
+func NewProxy(remoteUrl string) (*Proxy, error) {
+	url, err := url.Parse(remoteUrl)
+	if err != nil {
+		return nil, err
+	}
+	return &Proxy{rp:httputil.NewSingleHostReverseProxy(url)}, nil
+}
+
+func (p *Proxy)Handle(w http.ResponseWriter, r *http.Request) {
+	p.rp.ServeHTTP(w, r)
+}
+
