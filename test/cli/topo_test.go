9add90644d561a064779991cfa226845f7fec30a
ChaosXu <jun.chaos@gmail.com>
2017-02-10 16:04:47 +0800

    test: nerv-cli topo

--- test/cli/topo_test.go
+++ test/cli/topo_test.go
@@ -5,8 +5,72 @@ import (
 	"regexp"
 )
 
-func TestNervCmd(t *testing.T) {
+func TestTopo(t *testing.T) {
+	nerv:=setup(t)
+	defer clear(t,nerv)
 
+	//create
+	cmd := &Cmd{
+		Dir: "../../release/nerv/nerv-cli/bin",
+		Cli:"./nerv-cli",
+		Items:[]string{"topo", "create", "-t", "/nerv/worker_cluster.json", "-o", "topo-1","-n ../../../../test/cli/topo_test_inputs.json"},
+	}
+
+	var id string
+	if out, err := cmd.Run(t); err != nil {
+		t.Log(string(out))
+		t.Errorf("%s", err.Error())
+	} else {
+		res := string(out)
+		t.Log(res)
+		regex := regexp.MustCompile(`.*\s([0-9]+),.*`)
+		id = regex.FindStringSubmatch(res)[1]
+	}
+
+	//list
+	cmd = &Cmd{
+		Dir: "../../release/nerv/nerv-cli/bin",
+		Cli:"./nerv-cli",
+		Items:[]string{"topo", "list"},
+	}
+
+	if out, err := cmd.Run(t); err != nil {
+		t.Log(string(out))
+		t.Errorf("%s", err.Error())
+	} else {
+		t.Log(string(out))
+	}
+
+	//get
+	cmd = &Cmd{
+		Dir: "../../release/nerv/nerv-cli/bin",
+		Cli:"./nerv-cli",
+		Items:[]string{"topo", "get", "-i", id},
+	}
+
+	if out, err := cmd.Run(t); err != nil {
+		t.Log(string(out))
+		t.Errorf("%s", err.Error())
+	} else {
+		t.Log(string(out))
+	}
+
+	//delete
+	cmd = &Cmd{
+		Dir: "../../release/nerv/nerv-cli/bin",
+		Cli:"./nerv-cli",
+		Items:[]string{"topo", "delete", "-i", id},
+	}
+
+	if out, err := cmd.Run(t); err != nil {
+		t.Log(string(out))
+		t.Errorf("%s", err.Error())
+	} else {
+		t.Log(string(out))
+	}
+}
+
+func setup(t *testing.T) string {
 	//create
 	cmd := &Cmd{
 		Dir: "../../release/nerv/nerv-cli/bin",
@@ -55,6 +119,7 @@ func TestNervCmd(t *testing.T) {
 		t.Log(string(out))
 	}
 
+
 	//start
 	cmd = &Cmd{
 		Dir: "../../release/nerv/nerv-cli/bin",
@@ -69,8 +134,12 @@ func TestNervCmd(t *testing.T) {
 		t.Log(string(out))
 	}
 
+	return id
+}
+
+func clear(t *testing.T, id string) {
 	//stop
-	cmd = &Cmd{
+	cmd := &Cmd{
 		Dir: "../../release/nerv/nerv-cli/bin",
 		Cli:"./nerv-cli",
 		Items:[]string{"nerv", "stop", "-i", id},
--- test/golang/reg_test.go
+++ test/golang/reg_test.go
@@ -10,4 +10,13 @@ func TestReg(t *testing.T) {
 
 	reg := regexp.MustCompile(`^\$\{(.+)\}$`)
 	fmt.Println(reg.FindStringSubmatch("${a_b+c}")[1])
+
+	regex := regexp.MustCompile(`.*\t([0-9]+).*`)
+	id := regex.FindStringSubmatch(`
+	[
+		123,
+		null
+	]
+	`)[1]
+	fmt.Println(id)
 }
