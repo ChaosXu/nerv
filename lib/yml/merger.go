60cf3efe33113b7462db09612eae66b595912f8b
ChaosXu <jun.chaos@gmail.com>
2017-03-06 17:41:23 +0800

    test merger

--- lib/yml/merger.go
+++ lib/yml/merger.go
@@ -3,9 +3,10 @@ package yml
 import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
-	"reflect"
 	"fmt"
+	"os"
 )
+
 
 // Merger merge multi yml file in one
 type Merger struct {
@@ -37,7 +38,7 @@ func (p *Merger) Remove(file string) string {
 
 // Merge all files into one
 func (p *Merger) Merge() error {
-	sourceYml := map[string]interface{}{}
+	sourceYml := map[interface{}]interface{}{}
 	for _, file := range p.files {
 		temp, err := p.load(file)
 		if err != nil {
@@ -47,66 +48,100 @@ func (p *Merger) Merge() error {
 			return err
 		}
 	}
-	targetYml, err := p.load(p.target)
+	out, err := yaml.Marshal(&sourceYml)
 	if err != nil {
 		return err
+	} else {
+		ioutil.WriteFile(p.target, out, os.ModePerm)
 	}
-	p.appendUpdateDelete(targetYml, sourceYml)
 	return nil
 }
 
-func (p *Merger) load(file string) (map[string]interface{}, error) {
+func (p *Merger) load(file string) (map[interface{}]interface{}, error) {
 	data, err := ioutil.ReadFile(file)
 	if err != nil {
 		return nil, err
 	}
-	out := &map[string]interface{}{}
-	if err := yaml.Unmarshal(data, out); err != nil {
+	out := map[interface{}]interface{}{}
+	if err := yaml.Unmarshal(data, &out); err != nil {
 		return nil, err
 	}
+
 	return out, nil
 }
 
-func (p *Merger) appendUpdate(target map[string]interface{}, source map[string]interface{}) error {
-	for k, v := range target {
-		sv := source[k]
-		if sv != nil {
-			targetMap, ok := v.(map[string]interface{})
-			if !ok {
-				target[k] = sv
+func (p *Merger) appendUpdate(target map[interface{}]interface{}, source map[interface{}]interface{}) error {
+	for k, v := range source {
+		tv := target[k]
+		if tv != nil {
+			sourceMap, sok := v.(map[interface{}]interface{})
+			if sok {
+				targetMap, tok := tv.(map[interface{}]interface{})
+				if tok {
+					p.appendUpdate(targetMap, sourceMap)
+				} else {
+					return fmt.Errorf("target %s isn't map", k)
+				}
+			} else {
+				sourceArray, sok := v.([]interface{})
+				if sok {
+					targetArray, tok := tv.([]interface{})
+					if tok {
+						for _, item := range sourceArray {
+							targetArray = append(targetArray, item)
+						}
+						target[k] = targetArray
+					} else {
+						return fmt.Errorf("target %s isn't array", k)
+					}
+				} else {
+					target[k] = v
+				}
 			}
-			sourceMap, ok := sv.(map[string]interface{})
-			if !ok {
-				return fmt.Errorf("the type of %s in source isn't map", k)
-			}
-			p.appendUpdate(targetMap, sourceMap)
-		}
-	}
-	return nil
-}
 
-func (p *Merger) appendUpdateDelete(target map[string]interface{}, source map[string]interface{}) error {
-	removed := []string{}
-	for k, v := range target {
-		sv := source[k]
-		if sv != nil {
-			targetMap, ok := v.(map[string]interface{})
-			if !ok {
-				target[k] = sv
-			}
-			sourceMap, ok := sv.(map[string]interface{})
-			if !ok {
-				return fmt.Errorf("the type of %s in source isn't map", k)
-			}
-			p.appendUpdateDelete(targetMap, sourceMap)
 		} else {
-			removed = append(removed, k)
+			target[k] = v
 		}
 	}
 
-	for _, k := range removed {
-		delete(target, k)
-	}
 	return nil
 }
+
+//func (p *Merger) appendUpdateDelete(target map[interface{}]interface{}, source map[interface{}]interface{}) error {
+//	removed := []interface{}{}
+//	for k, v := range target {
+//		sv := source[k]
+//		if sv != nil {
+//			targetMap, tok := v.(map[interface{}]interface{})
+//			if tok {
+//				sourceMap, sok := sv.(map[interface{}]interface{})
+//				if sok {
+//					p.appendUpdateDelete(targetMap, sourceMap)
+//				} else {
+//					return fmt.Errorf("the type of %s in source isn't map", k)
+//				}
+//			} else {
+//				targetArray, tok := v.([]interface{})
+//				if tok {
+//					sourceArray, sok := sv.([]interface{})
+//					if sok {
+//
+//					} else {
+//						return fmt.Errorf("the type of %s in source isn't array", k)
+//					}
+//				} else {
+//					target[k] = sv
+//				}
+//			}
+//
+//		} else {
+//			removed = append(removed, k)
+//		}
+//	}
+//
+//	for _, k := range removed {
+//		delete(target, k)
+//	}
+//	return nil
+//}
 
---
+++ lib/yml/merger_test.go
@@ -0,0 +1,122 @@
+package yml
+
+import (
+	"testing"
+	"k8s.io/kubernetes/staging/src/k8s.io/client-go/_vendor/gopkg.in/yaml.v2"
+	"io/ioutil"
+	"fmt"
+	"reflect"
+)
+
+func TestMerger(t *testing.T) {
+
+	merger := NewMerger("merged.yml")
+	merger.Add("source1.yml")
+	merger.Add("source2.yml")
+	merger.Merge()
+
+	t2, err := loadFile("merged.yml")
+	if err != nil {
+		t.Error(err)
+	}
+
+	t1, err := loadFile("target.yml")
+	if err != nil {
+		t.Error(err)
+	}
+
+	//if !reflect.DeepEqual(t1,t2) {
+	//
+	//}
+	if err := equals(t1, t2); err != nil {
+		t.Error(err)
+	}
+}
+
+func loadFile(file string) (map[string]interface{}, error) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	out := map[string]interface{}{}
+	if err := yaml.Unmarshal(data, out); err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func equals(t map[string]interface{}, s map[string]interface{}) error {
+	for tk, tv := range t {
+		sv := s[tk]
+		if sv == nil {
+			return fmt.Errorf("could not found %s in source map", tk)
+		}
+
+		tmap, tok := tv.(map[string]interface{})
+		if tok {
+			smap, sok := sv.(map[string]interface{})
+			if sok {
+				if err := equals(tmap, smap); err != nil {
+					return fmt.Errorf("%s. parent:%s", err.Error(), tk)
+				}
+			} else {
+				return fmt.Errorf("the type of %s isn't equals", tk)
+			}
+		} else {
+			tarray, tok := tv.([]interface{})
+			if tok {
+				sarray, sok := sv.([]interface{})
+				if sok {
+					if err := equalsArray(tarray, sarray); err != nil {
+						return fmt.Errorf("%s. parent:%s", err.Error(), tk)
+					}
+				}
+			} else {
+				if !equalsValue(tv, sv) {
+					return fmt.Errorf("don't equals. index:%v", tk)
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func equalsArray(t []interface{}, s []interface{}) error {
+	if len(t) != len(s) {
+		return fmt.Errorf("target len is %v,source len is %v", len(t), len(s))
+	}
+
+	for i, tv := range t {
+		sv := s[i]
+		tmap, tok := tv.(map[string]interface{})
+		if tok {
+			smap, sok := sv.(map[string]interface{})
+			if sok {
+				if err := equals(tmap, smap); err != nil {
+					return fmt.Errorf("%s,index:%v", err.Error(), i)
+				}
+			} else {
+				return fmt.Errorf("source isn't map,index:%v", i)
+			}
+		} else {
+			tarray, tok := tv.([]interface{})
+			if tok {
+				sarray, sok := sv.([]interface{})
+				if sok {
+					if err := equalsArray(tarray, sarray); err != nil {
+						return fmt.Errorf("%s. index:%v", err.Error(), i)
+					}
+				}
+			} else {
+				if !equalsValue(tv, sv) {
+					return fmt.Errorf("don't equals. index:%v", i)
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func equalsValue(t, s interface{}) bool {
+	return reflect.DeepEqual(t, s)
+}
